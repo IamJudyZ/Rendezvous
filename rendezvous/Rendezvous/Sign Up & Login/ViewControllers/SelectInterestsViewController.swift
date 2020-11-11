dc6f8ade852c04d8bac5caf3ecaa5a64415d5f21
Bing Chen <bc2486@nyu.edu>
2020-11-11 15:16:01 -0500

    update collection view layout for better looking cells

--- rendezvous/Rendezvous/Sign Up & Login/ViewControllers/SelectInterestsViewController.swift
+++ rendezvous/Rendezvous/Sign Up & Login/ViewControllers/SelectInterestsViewController.swift
@@ -12,6 +12,14 @@ import UIKit
 class SelectInterestsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet var collectionView: UICollectionView!
+    
+    let columnLayout = CustomViewFlowLayout()
+    var interestsCatalog = ["vegan", "women in tech", "elections", "music", "gaming",
+                            "running", "gym", "dogs", "cats", "technology",
+                            "basketball", "football", "chinese food", "beach",
+                            "traveling", "baseball", "italian food", "daredevil",
+                            "trivia", "cycling", "cars", "kids"]
+    
     var currentUser: User!
     
     override func viewDidLoad() {
@@ -22,48 +30,51 @@ class SelectInterestsViewController: UIViewController, UICollectionViewDelegate,
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        // TODO: sample layout stuff Bing needs to work with after Ramses finishes backend
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 30, height: 20)
-        collectionView.collectionViewLayout = layout
+        //Setup CollectionView Auto-Layout
+        if #available(iOS 10.0, *) {
+            columnLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+        else {
+            columnLayout.estimatedItemSize = CGSize(width: 128, height: 20)
+        }
+        collectionView.collectionViewLayout = columnLayout
+    }
+    
+    func getInterest(for indexPath: IndexPath) -> String {
+        return interestsCatalog[indexPath.row]
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //When an item is selected
         //First deselect what was selected before
-        collectionView.deselectItem(at: indexPath, animated: true)
+        //collectionView.deselectItem(at: indexPath, animated: true)
         // temporary sample action
         print("you tapped me")
         
-        // BING: This is where you implement what happens when an interest item is selected
+        if let cell = collectionView.cellForItem(at: indexPath) as? InterestCell {
+//            cell.contentView.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+//            print(cell)
+            cell.backgroundColor = UIColor.blue
+        }
+        // BING: This is where you implement what happens when an interest item is selected => indexPath.item
         // should be added to the user's profile
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //Num of cells
-        //BING: should be changed to be the size of interests list imported from database
-        return 100
+        return interestsCatalog.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: InterestCell.identifier, for: indexPath) as! InterestCell
         
-        //BING: UITextField should be the name of the interest. Uncomment once UITextField becomes available
+        let interestTextField = UITextField()
+        interestTextField.text = getInterest(for: indexPath)
         // configure is a custom function in InterestCell.swift
-//        cell.configure(with: UITextField)
+        cell.configure(with: interestTextField)
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        //Setup layout for cell
-        // TODO: fine tune layout after Ramses is done with backend
-        return CGSize(width: 30, height: 20)
-    }
-    
-   
-    
-
     /*
     // MARK: - Navigation
 
@@ -75,29 +86,3 @@ class SelectInterestsViewController: UIViewController, UICollectionViewDelegate,
     */
 
 }
-
-
-// BING: Ignore all this, it is repeated above
-//extension ViewController: UICollectionViewDelegate {
-//    
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        //When an item is selected
-//        //First deselect what was selected before
-//        collectionView.deselectItem(at: indexPath, animated: true)
-//        print("you tapped me")
-//    }
-//}
-
-//extension ViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 12
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
-//        return cell
-//    }
-//}
-
-//extension ViewController: UICollectionViewDelegateFlowLayout {}
-
