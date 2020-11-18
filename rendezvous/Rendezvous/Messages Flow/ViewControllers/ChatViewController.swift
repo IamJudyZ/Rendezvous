76117a22161703eea89bea8b502d2a73e79a1f48
IamJudyZ <judy@zhang.me>
2020-11-18 15:36:08 -0800

    added matches page with dummy text

---
+++ rendezvous/Rendezvous/Messages Flow/ViewControllers/ChatViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ChatViewController.swift
+//  Rendezvous
+//
+//  Created by Judy Zhang on 11/18/20.
+//  Copyright © 2020 NYUiOS. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+    
+    
+}
+
+struct Sender: SenderType {
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+}
+
+class ChatViewController: MessagesViewController {
+
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Joe Smith")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello World message")))
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello World message, Hello World message")))
+
+        //view.backgroundColor = .yellow
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+
+
+
+}
+
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    
+}
+
